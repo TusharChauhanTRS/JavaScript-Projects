0a5eb2407fd41391ff1190dc46e5775130517bf3
Tushar Chauhan <60914862+TusharChauhanTRS@users.noreply.github.com>
2023-06-30 12:58:49 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -17,7 +17,7 @@ arr.forEach(buttons => {
             string = string.substring(0,string.length-1);
             input.value = string;
         }
-        // else if(e.target.innerHTML == '')
+       
         else{
             string += e.target.innerHTML;
             input.value = string;
